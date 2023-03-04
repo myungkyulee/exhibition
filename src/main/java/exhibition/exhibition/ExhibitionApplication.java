359ac4b33e14e43976bbc1f349811e9f2d5f8519
Myungkyulee <skdlaudrb@naver.com>
2023-03-04 18:38:18 +0900

    init : initialize project

---
+++ src/main/java/exhibition/exhibition/ExhibitionApplication.java
@@ -0,0 +1,11 @@
+package exhibition.exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionApplication.class, args);
+	}
+}
